3276e01e3261768d4cfbadcab0bedc877608fdfb
panminglong <652645687@qq.com>
2017-03-02 22:30:42 +0800

    https://github.com/panminglong/myku

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+<head>
+  <meta charset="UTF-8">
+  <title>凯题租车</title>
+</head>
+<body style="background-color:#cccccc;width:100%;height:100%">
+
+ <div >
+   <br>
+  <h3>凯题租车电商后台管理系统</h3>
+  <br>
+  <br>
+  <div>
+    <table width="100%"border="0"cellpadding="5"cellspacing="1"bgcolor="black"algin="center">
+    <tr bgcolor="#fffff0">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td>操作</td>
+    </tr>
+    <tr bgcolor="#ffffbb">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#fffff0">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#ffffbb">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#fffff0">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#ffffbb">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#fffff0">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    <tr bgcolor="#ffffbb">
+    <td>用户</td>
+    <td>性别</td>
+    <td>年龄</td>
+    <td>等级</td>
+    <td>地址</td>
+    <td>电话号码</td>
+    <td>消费金额</td>
+    <td><a href="#">修改<a> <a href="#">删除<a></td>
+    </tr>
+    
+  </table>
+  </div>
+  </div>
+</body>
+</html>
+?>
